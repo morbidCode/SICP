a7520e27be76c243288048b57d15e98e668ad191
morbidCode <morbidcode666@gmail.com>
2018-05-29 03:38:28 +0800

    added sicp-concurrency package

--- import.scm
+++ import.scm
@@ -3,3 +3,7 @@
 (require sicp)
 (print-as-expression #f)
 (#%require r5rs/init)
+
+; source: https://planet.racket-lang.org/display.ss?package=sicp-concurrency.plt&owner=dyoo
+
+(require (planet dyoo/sicp-concurrency:1:2/sicp-concurrency)) 
