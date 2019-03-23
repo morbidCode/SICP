8317fd3c1ca226991fc9be56746481cfcb1fb43d
morbidCode <morbidcode666@gmail.com>
2019-03-23 21:42:04 +0800

    refactor: number of arguments

--- 3.71.scm
+++ 3.71.scm
@@ -2,8 +2,9 @@
 
 (define (ramanujan-numbers)
 (define weights (stream-map (lambda (x) (weight (car x) (cadr x))) (weighted-pairs integers integers weight)))
-(define (generate s t)
-(if (= (stream-car s) (stream-car t))
-(cons-stream (stream-car s) (generate (stream-cdr s) (stream-cdr t)))
-(generate (stream-cdr s) (stream-cdr t))))
-(generate weights (stream-cdr weights)))
+
+(define (generate s)
+(if (= (stream-car s) (stream-car (stream-cdr s)))
+(cons-stream (stream-car s) (generate (stream-cdr s)))
+(generate (stream-cdr s))))
+(generate weights))
--- 3.72.scm
+++ 3.72.scm
@@ -2,8 +2,9 @@
 
 (define (sum-of-squares-3-ways)
 (define weights (stream-map (lambda (x) (weight (car x) (cadr x))) (weighted-pairs integers integers weight)))
-(define (generate s t)
-(if (= (stream-car s) (stream-car (stream-cdr s)) (stream-car t))
-(cons-stream (stream-car s) (generate (stream-cdr s) (stream-cdr t)))
-(generate (stream-cdr s) (stream-cdr t))))
-(generate weights (stream-cdr (stream-cdr weights))))
+
+(define (generate s)
+(if (= (stream-car s) (stream-car (stream-cdr s)) (stream-car (stream-cdr (stream-cdr s))))
+(cons-stream (stream-car s) (generate (stream-cdr s)))
+(generate (stream-cdr s))))
+(generate weights))
