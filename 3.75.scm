237cf472334618146a3e1bc5bc3dcd46d8e7646c
morbidCode <morbidcode666@gmail.com>
2019-03-26 03:00:57 +0800

    fix: exercises description misinterpreted

--- 3.75.scm
+++ 3.75.scm
@@ -1,12 +1,10 @@
 (define (make-zero-crossings 
-         input-stream last-value false-value?)
-(if false-value?
-  (make-zero-crossings (stream-cdr input-stream) (stream-car input-stream) false)
+         input-stream last-value last-avpt)
 (let ((avpt 
          (/ (+ (stream-car input-stream) 
                last-value) 
             2)))
     (cons-stream 
-     (sign-change-detector avpt last-value)
+     (sign-change-detector avpt last-avpt)
      (make-zero-crossings 
-      (stream-cdr input-stream) avpt false)))))
+      (stream-cdr input-stream) (stream-car input-stream) avpt))))
--- 3.76.scm
+++ 3.76.scm
@@ -4,6 +4,6 @@
 (/ (+ element next-element) 2)) 
 input-stream (stream-cdr input-stream)))
 
-(define smoothed-sense-data (smooth sense-data))
+(define smoothed-sense-data (cons-stream 0 (smooth sense-data)))
 (define zero-crossings 
   (make-zero-crossings smoothed-sense-data 0))
