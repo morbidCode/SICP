d7ece7110ea96bb57a3c6957307ec9e86523a90f
morbidCode <morbidcode666@gmail.com>
2019-04-02 03:08:04 +0800

    refactor: changed queens representation to regular list - exercise 2.42

--- 2.42.scm
+++ 2.42.scm
@@ -1,43 +1,15 @@
-(define (queens board-size)
-  (define (queen-cols k)
-    (if (= k 0)
-        (list empty-board)
-        (filter
-         (lambda (positions) 
-           (safe? k positions))
-         (flatmap
-          (lambda (rest-of-queens)
-            (map (lambda (new-row)
-                   (adjoin-position 
-                    new-row 
-                    k 
-                    rest-of-queens))
-                 (enumerate-interval 
-                  1 
-                  board-size)))
-          (queen-cols (- k 1))))))
-  (queen-cols board-size))
+(define empty-board nil)
 
-  (define empty-board nil)
+(define (adjoin-position new-row k rest-of-queens)
+(cons new-row rest-of-queens))
 
- (define queen cons)
-  (define row car)
-  (define col cdr)
-  
-  (define (adjoin-position new-row k rest-of-queens)
-    (accumulate cons
-                (list (queen new-row k))
-                rest-of-queens))
-  
-  (define (safe? k positions)
-    (define new (list-ref positions (- k 1)))
-    (define (compare lst)
-      (let ((current (car lst)))
-        (cond ((null? (cdr lst)) #t)
-              ((or (= (row new) (row current))
-                   (= (abs (- (row new) (row current)))
-                      (- (col new) (col current))))
-               #f)
-              (else (compare (cdr lst))))))
-    (compare positions))
-  
+(define (safe? k positions)
+(if (null? positions)
+true
+(let ((new-queen (car positions)))
+(define (unique-row? k positions)
+(cond ((null? positions) true)
+((= (car positions) new-queen) false)
+((= (abs (- (car positions) new-queen)) k) false)
+(else (unique-row? (+ k 1) (cdr positions)))))
+(unique-row? 1 (cdr positions)))))
